aced80ea3bfad84c9f5162afb546a3ef3d144fd2
danielsanchez68 <danielsanchez68@hotmail.com>
2023-02-28 15:53:25 -0300

    v0.1.1

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const multer = require('multer')
 const sharp = require('sharp')
 const fs = require('fs')
-const cors = require('cors')
 
 
 const storage = multer.diskStorage({
@@ -17,7 +16,6 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 const app = express()
-app.use(cors())
 
 app.use(express.static('public'))
 app.use(express.json())
